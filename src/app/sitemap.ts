72d5a337c3b7f3ae3078c2b9ce9f6b6973e08df6
themaroonmania-gif <themaroonmania@gmail.com>
2026-07-08 10:30:53 -0500

    static_export_config

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { calculators } from '@/config/calculators';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://tallywise.co';
 
